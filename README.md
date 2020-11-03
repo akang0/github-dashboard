6adc2af44584e3e582f36f2de4a1ac658d14eee5
Maggie Chen <chenz.4027@gmail.com>
2020-11-03 09:08:55 -0500

    create dashboard

--- README.md
+++ README.md
@@ -18,9 +18,24 @@ Mongodb BI allows the cubejs starts conversations with your mongodb.
 - Generate your CUBEJS_API_SECRET following the doc https://cube.dev/docs/security#generating-tokens. Replace the <CUBEJS_API_SECRET> placeholder in the .env file with your newly generated token
 - Copy the .env.template to .env. Your .env file contains placeholders in DB credentials. Please replace them with your DB credentials.
 
+- Please replace `CUBEJS_TOKEN` in dashboard-app/src/App.js with CUBEJS_API_SECRET in your .env
+
 # Start
+
+- To start the cube.js playground:
 
 ```Shell
 $ npm install
 $ npm run dev
 ```
+
+The cube.js playground will be running on localhost:3000
+
+- To start the dashboard
+
+```Shell
+$ cd dashboard-app
+$ npm start
+```
+
+The dashboard will be running on localhost:3000
---
+++ dashboard-app/src/App.js
@@ -0,0 +1,34 @@
+import './body.css';
+import 'antd/dist/antd.css';
+import React from 'react';
+import '@ant-design/compatible';
+import { Layout } from 'antd';
+import cubejs from '@cubejs-client/core';
+import { CubeProvider } from '@cubejs-client/react';
+import Header from './components/Header';
+import WebSocketTransport from '@cubejs-client/ws-transport';
+const API_URL = "http://localhost:4000";
+const CUBEJS_TOKEN = "";
+const cubejsApi = cubejs({
+  transport: new WebSocketTransport({
+    authorization: CUBEJS_TOKEN,
+    apiUrl: API_URL.replace('http', 'ws')
+  })
+});
+
+const AppLayout = ({
+  children
+}) => <Layout style={{
+  height: '100%'
+}}>
+    <Header />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>;
+
+const App = ({
+  children
+}) => <CubeProvider cubejsApi={cubejsApi}>
+    <AppLayout>{children}</AppLayout>
+  </CubeProvider>;
+
+export default App;
